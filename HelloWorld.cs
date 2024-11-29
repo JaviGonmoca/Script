dc5190af7a8a0a5e2e2ba5e18f103c4ec7fba709
JaviGonmoca <javigonmoca@gmail.com>
2024-11-29 17:02:32 +0100

    Add files via upload

---
+++ HelloWorld.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+   void Start()
+    {
+        print("Hola, soy Pedro, mi color favorito es el 9 y mi número favorito es el azul :)");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
